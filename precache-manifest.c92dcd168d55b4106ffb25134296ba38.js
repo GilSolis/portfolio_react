cc0c6f34fec9aad3ebde992d7402273afacc20e4
Gil Solis <gilsolisuw@gmail.com>
2020-05-16 22:55:22 -0700

    Updates

---
+++ precache-manifest.c92dcd168d55b4106ffb25134296ba38.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdfba14cf3d1339c9573d03dd0a26d26",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "8c97ba883af5e689a019",
+    "url": "/portfolio_react/static/css/main.c7f48390.chunk.css"
+  },
+  {
+    "revision": "24b30c8b2da132da6fe7",
+    "url": "/portfolio_react/static/js/2.6ec7845c.chunk.js"
+  },
+  {
+    "revision": "e4279675a9c60179159c82343fcf3639",
+    "url": "/portfolio_react/static/js/2.6ec7845c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c97ba883af5e689a019",
+    "url": "/portfolio_react/static/js/main.0e63f6fc.chunk.js"
+  },
+  {
+    "revision": "a43d4d96565b46a8fd88",
+    "url": "/portfolio_react/static/js/runtime-main.d4c6cf8a.js"
+  },
+  {
+    "revision": "42b1f0e0d08b25eac360ac2730081223",
+    "url": "/portfolio_react/static/media/GilbertSolisResume.42b1f0e0.pdf"
+  },
+  {
+    "revision": "6ac015325130dcfda39afa1f8dfc6848",
+    "url": "/portfolio_react/static/media/avatar.6ac01532.png"
+  },
+  {
+    "revision": "2a3c3dddee21862c7a80be2836ff2189",
+    "url": "/portfolio_react/static/media/burger.2a3c3ddd.JPG"
+  },
+  {
+    "revision": "f33ec7b4bcb06848b30ecd18ae02e68b",
+    "url": "/portfolio_react/static/media/championsleague.f33ec7b4.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio_react/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "61ba1c8095a948c8ed23d349d264622a",
+    "url": "/portfolio_react/static/media/covidnstuff.61ba1c80.jpg"
+  },
+  {
+    "revision": "bda712f514a7d69b453e74a353a4fc64",
+    "url": "/portfolio_react/static/media/password.bda712f5.jpg"
+  }
+]);
