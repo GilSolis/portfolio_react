194c21baf55ee334eea200a77560d589e1476f49
Gil Solis <gilsolisuw@gmail.com>
2020-05-18 13:08:40 -0700

    navbar link update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -48,7 +48,7 @@ const menuItems = [
     {
         listIcon: <Home />,
         listText: "Home",
-        listPath: "/"
+        listPath: "/portfolio_react"
     },
     {
         listIcon: <AssignmentInd />,
